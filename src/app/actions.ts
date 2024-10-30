5c9172b2c1062b56056ec80814c9347b62644267
Yohendry <yohendry.hurtado@tafitech.io>
2024-10-30 17:26:43 -0500

    mejora

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,27 +2,37 @@
 import axios from "axios";
 import sharp from "sharp";
 
-export async function convert(prevState: string | null, formData: FormData) {
-  console.log({
-    file: formData.get("file"),
-    type: formData.get("type"),
-    prevState,
-  });
+async function generate(buffer: ArrayBuffer, factor: number, blur: number) {
+  const metadata = await sharp(buffer).metadata();
 
+  const width = metadata.width
+    ? Math.round((metadata.width / 100) * factor)
+    : 30;
+
+  const webp = sharp(buffer).resize({ width }).blur(blur).webp({ quality: 30 });
+
+  const res = await webp.toBuffer();
+  const base64 = Buffer.from(res).toString("base64");
+
+  return `data:image/webp;base64,${base64}`;
+}
+
+export async function convert(formData: FormData): Promise<string | null> {
   if (!formData.get("file")) return null;
 
   const inputType = formData.get("type");
   const factor = parseInt(formData.get("factor") as string);
+  const blur = parseInt(formData.get("blur") as string);
 
   let buffer: ArrayBuffer;
 
   if (inputType === "file") {
     const file = formData.get("file") as File;
-    if (file.size === 0) throw "no se selecciono un archivo";
+    if (file.size === 0) return null;
     buffer = await file.arrayBuffer();
   } else {
     const url = formData.get("file") as string;
-    if (!url) throw "no se selecciono un archivo";
+    if (!url) return null;
 
     buffer = (
       await axios({
@@ -32,16 +42,7 @@ export async function convert(prevState: string | null, formData: FormData) {
     ).data as ArrayBuffer;
   }
 
-  const metadata = await sharp(buffer).metadata();
+  const base64 = await generate(buffer, factor, blur);
 
-  const newW = metadata.width
-    ? Math.round((metadata.width / 100) * factor)
-    : 30;
-
-  console.log(newW);
-  const res = await sharp(buffer).resize(newW).blur(5).webp().toBuffer();
-
-  const base64 = Buffer.from(res).toString("base64");
-
-  return `data:image/webp;base64,${base64}`;
+  return base64;
 }
--- src/components/upload-form.tsx
+++ src/components/upload-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { convert } from "@/app/actions";
-import { useActionState, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { Input } from "./ui/input";
 import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
 import { Button } from "./ui/button";
@@ -18,18 +18,32 @@ const FileTypeMap: Record<FileType, string> = {
 
 export default function UploadForm() {
   const [type, setType] = useState<FileType>("file");
-  const [factor, setFactor] = useState<number[]>([15]);
-  const [state, formAction] = useActionState(convert, null);
+  const formRef = useRef<HTMLFormElement>(null);
+  const [result, setResult] = useState<string | null>(null);
+  const [pending, setPending] = useState<boolean>(false);
 
-  console.log({ state });
+  const submit = useCallback(async () => {
+    setPending(true);
+    if (formRef.current) {
+      const result = await convert(new FormData(formRef.current));
+
+      if (result) setResult(result);
+    }
+    setPending(false);
+  }, []);
+
   return (
-    <form className="w-full gap-8 flex flex-col" action={formAction}>
+    <form className="w-full gap-8 flex flex-col" ref={formRef}>
       <input type="hidden" value={type} name="type" />
       <div className="w-full">
         <ToggleGroup
+          disabled={pending}
           type="single"
           value={type}
-          onValueChange={(value: FileType) => setType(value)}
+          onValueChange={(value: FileType) => {
+            if (value) setType(value);
+          }}
+          defaultValue="file"
         >
           <ToggleGroupItem value="file" name="type">
             Archivo
@@ -41,40 +55,62 @@ export default function UploadForm() {
       </div>
 
       <div className="flex w-full gap-2">
-        <Input type={FileTypeMap[type]} className="w-full" name="file"></Input>
-        <Button type="submit">Convertir</Button>
-      </div>
-
-      <div className="flex gap-2">
-        <span className="font-mono w-[60px]">{factor[0]} %</span>
-        <Slider
-          defaultValue={factor}
-          onValueChange={(value) => setFactor(value)}
-          max={100}
-          step={1}
-          min={1}
-          name="factor"
+        <Input
+          disabled={pending}
+          type={FileTypeMap[type]}
+          className="w-full"
+          name="file"
+          onChange={submit}
         />
       </div>
-      {state && (
+
+      <div className="flex gap-2 items-center">
+        <span className="font-mono w-[80px]">escala:</span>
+        <div className="flex-1">
+          <Slider
+            disabled={pending}
+            defaultValue={[15]}
+            max={100}
+            step={1}
+            min={1}
+            name="factor"
+            onChange={submit}
+          />
+        </div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <span className="font-mono w-[80px]">blur: </span>
+        <div className="flex-1">
+          <Slider
+            disabled={pending}
+            defaultValue={[5]}
+            max={100}
+            step={1}
+            min={1}
+            name="blur"
+            onChange={submit}
+          />
+        </div>
+      </div>
+      {result && (
         <div className="w-full flex flex-col gap-4">
           <span>Resultado: </span>
           <div className="relative h-56">
             <Image
               alt="imagen generada"
               fill
-              src={state}
+              src={result}
               style={{ objectFit: "contain" }}
             />
           </div>
 
           <p className="break-all text-wrap bg-muted font-mono text-sm font-semibold">
-            {state}
+            {result}
           </p>
 
           <Button
             type="button"
-            onClick={() => navigator.clipboard.writeText(state)}
+            onClick={() => navigator.clipboard.writeText(result)}
           >
             copiar <CopyIcon />
           </Button>
